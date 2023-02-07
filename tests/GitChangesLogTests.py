367124a4111269b59e4ecb57266b289c967d07c3
klee0kai <klee0kai@gmail.com>
2023-02-07 21:11:34 +0400

    rename project

--- tests/GitChangesLogTests.py
+++ tests/GitChangesLogTests.py
@@ -6,6 +6,9 @@ from ci_tools.github_tools_pack import GithubToolsPack
 
 class GitChangesLogTests(TestCase):
 
+    def test_cwd(self):
+        print(f"cur path {os.getcwd()}")
+
     def test_log_parse(self):
         os.chdir(f"{os.getcwd()}/tests/some_git_rep")
         print(f"cur path {os.getcwd()}")
